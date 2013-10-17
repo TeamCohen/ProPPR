2cca2fa44080f1b204dcd510f456c0b6e73ca5ff
Kathryn Rivard <krivard@cs.cmu.edu>
2013-10-17 12:30:05 -0400

    neo4j

--- src/edu/cmu/ml/praprolog/prove/Neo4jGraphComponent.java
+++ src/edu/cmu/ml/praprolog/prove/Neo4jGraphComponent.java
@@ -18,20 +18,21 @@ import org.neo4j.graphdb.RelationshipType;
 import org.neo4j.graphdb.Transaction;
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
 import org.neo4j.graphdb.index.Index;
+import org.neo4j.graphdb.index.IndexHits;
 import org.neo4j.graphdb.index.RelationshipIndex;
 
 public class Neo4jGraphComponent extends GraphlikeComponent {
 	private static final Logger log = Logger.getLogger(Neo4jGraphComponent.class);
-//	private static enum RelTypes implements RelationshipType {
-//	    FUNCTOR
-//	}
+	//	private static enum RelTypes implements RelationshipType {
+	//	    FUNCTOR
+	//	}
 	private static final String NODENAME_KEY = "name";
 	private static final String FUNCTOR_KEY = "name";
-	
+
 	private static GraphDatabaseService graphDb;
 	private static Index<Node> nodeIndex;
 	private static RelationshipIndex relIndex;
-	
+
 	private static void init(String dbPath) {
 		if (graphDb != null) return;
 		graphDb = new GraphDatabaseFactory().newEmbeddedDatabase( dbPath );
@@ -39,7 +40,7 @@ public class Neo4jGraphComponent extends GraphlikeComponent {
 		relIndex = graphDb.index().forRelationships( "relationships" );
 		registerShutdownHook( graphDb );
 	}
-	
+
 	public Neo4jGraphComponent(String dbPath) {
 		init(dbPath);
 	}
@@ -50,25 +51,32 @@ public class Neo4jGraphComponent extends GraphlikeComponent {
 		try
 		{
 			this._tx_indexAppend(functor, src, dst);
-		    tx.success();
+			tx.success();
 		}
 		finally
 		{
-		    tx.finish();
+			tx.finish();
 		}
 	}
-	 private void _tx_indexAppend(String functor, Argument src, Argument dst) {
-		 Node firstNode = graphDb.createNode();
-			firstNode.setProperty( NODENAME_KEY, src.getName() );
-	        nodeIndex.add( firstNode, NODENAME_KEY, src.getName() );
-			Node secondNode = graphDb.createNode();
-			secondNode.setProperty( NODENAME_KEY, dst.getName() );
-	        nodeIndex.add( secondNode, NODENAME_KEY, src.getName() );
-			 
-			Relationship relationship = firstNode.createRelationshipTo( secondNode, DynamicRelationshipType.withName(functor) );
-			relationship.setProperty( FUNCTOR_KEY, functor );
-			relIndex.add( relationship, FUNCTOR_KEY, functor);
-	 }
+	private void _tx_indexAppend(String functor, Argument src, Argument dst) {
+		Node srcNode = getOrMakeNode(src.getName());
+		Node dstNode = getOrMakeNode(dst.getName());
+
+		Relationship relationship = srcNode.createRelationshipTo( dstNode, DynamicRelationshipType.withName(functor) );
+		relationship.setProperty( FUNCTOR_KEY, functor );
+		relIndex.add( relationship, FUNCTOR_KEY, functor);
+	}
+	private Node getOrMakeNode(String name) {
+		IndexHits<Node> hits = nodeIndex.get(NODENAME_KEY, name);
+		if (hits.hasNext()) {
+			return hits.getSingle();
+		} else {
+			Node ret = graphDb.createNode();
+			ret.setProperty( NODENAME_KEY, name );
+			nodeIndex.add( ret, NODENAME_KEY, name );
+			return ret;
+		}
+	}
 
 	@Override
 	protected boolean _indexContains(String functor) {
@@ -77,20 +85,20 @@ public class Neo4jGraphComponent extends GraphlikeComponent {
 
 	@Override
 	protected List<Argument> _indexGet(String functor, Argument srcConst) {
-        Node srcNode = nodeIndex.get( NODENAME_KEY, srcConst.getName() ).getSingle();
-        ArrayList<Argument> ret = new ArrayList<Argument>();
-        for (Relationship r : srcNode.getRelationships(DynamicRelationshipType.withName(functor), Direction.OUTGOING)) {
-        	ret.add(new ConstantArgument((String) r.getEndNode().getProperty(NODENAME_KEY)));
-        }
-        return ret;
+		Node srcNode = nodeIndex.get( NODENAME_KEY, srcConst.getName() ).getSingle();
+		ArrayList<Argument> ret = new ArrayList<Argument>();
+		for (Relationship r : srcNode.getRelationships(DynamicRelationshipType.withName(functor), Direction.OUTGOING)) {
+			ret.add(new ConstantArgument((String) r.getEndNode().getProperty(NODENAME_KEY)));
+		}
+		return ret;
 	}
 
 	@Override
 	protected int _indexGetDegree(String functor, Argument srcConst) {
-        Node srcNode = nodeIndex.get( NODENAME_KEY, srcConst.getName() ).getSingle();
-        int count=0;
-        for (Relationship r : srcNode.getRelationships(DynamicRelationshipType.withName(functor), Direction.OUTGOING)) count++;
-        return count;
+		Node srcNode = nodeIndex.get( NODENAME_KEY, srcConst.getName() ).getSingle();
+		int count=0;
+		for (Relationship r : srcNode.getRelationships(DynamicRelationshipType.withName(functor), Direction.OUTGOING)) count++;
+		return count;
 	}
 
 	@Override
@@ -104,7 +112,7 @@ public class Neo4jGraphComponent extends GraphlikeComponent {
 			System.err.println("Usage:\n\tdbPath graphFile functor arg1\n");
 			System.exit(0);
 		}
-		
+
 		String dbPath = args[0],
 				graphFile = args[1],
 				functor = args[2],
@@ -123,7 +131,7 @@ public class Neo4jGraphComponent extends GraphlikeComponent {
 			System.out.println(a);
 			t2 = System.currentTimeMillis();
 		}
-		
+
 	}
 
 	/**
@@ -138,15 +146,17 @@ public class Neo4jGraphComponent extends GraphlikeComponent {
 		Transaction tx = graphDb.beginTx();
 		try {
 			LineNumberReader reader = new LineNumberReader(new FileReader(graphFile));
-			
-			
+
 			String line;
-			long last = System.currentTimeMillis(), now = System.currentTimeMillis(), first = last;
+			long last = System.currentTimeMillis(), now = last, first = last;
+			log.info("Read "+reader.getLineNumber()+" lines "+now+" "+((double) reader.getLineNumber() / (now-first) * 1000)+" lps");
+			log.debug("mem "+now+" "+Runtime.getRuntime().totalMemory()+" "+(Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()));
 			while ((line=reader.readLine())!= null) {
 				if(reader.getLineNumber() % 10000 == 0) { tx.success(); tx.finish(); tx = graphDb.beginTx(); }
 				if( (now = System.currentTimeMillis()) - last > 2000)  {
-					log.info("Read "+reader.getLineNumber()+" lines ("+((double) reader.getLineNumber() / (now-first) * 1000)+" lps)");
+					log.info("Read "+reader.getLineNumber()+" lines "+now+" "+((double) reader.getLineNumber() / (now-first) * 1000)+" lps");
 					last = now;
+					log.debug("mem "+now+" "+Runtime.getRuntime().totalMemory()+" "+(Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()));
 				}
 				line=line.trim();
 				if (line.startsWith("#") || line.length()==0) continue;
@@ -157,8 +167,12 @@ public class Neo4jGraphComponent extends GraphlikeComponent {
 				String dst = parts[2].trim();
 				result._tx_indexAppend(edgeLabel, Argument.fromString(src), Argument.fromString(dst));
 			}
+			now = System.currentTimeMillis();
+			log.info("Read "+reader.getLineNumber()+" lines "+now+" "+((double) reader.getLineNumber() / (now-first) * 1000)+" lps");
+			log.debug("mem "+Runtime.getRuntime().totalMemory()+" "+(Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()));
+
 			reader.close();
-		    tx.success();
+			tx.success();
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -168,24 +182,24 @@ public class Neo4jGraphComponent extends GraphlikeComponent {
 		}
 		finally
 		{
-		    tx.finish();
+			tx.finish();
 		}
 		return result;
 	}
-	
-	
-    private static void registerShutdownHook( final GraphDatabaseService graphDb )
-    {
-        // Registers a shutdown hook for the Neo4j instance so that it
-        // shuts down nicely when the VM exits (even if you "Ctrl-C" the
-        // running application).
-        Runtime.getRuntime().addShutdownHook( new Thread()
-        {
-            @Override
-            public void run()
-            {
-                graphDb.shutdown();
-            }
-        } );
-    }
+
+
+	private static void registerShutdownHook( final GraphDatabaseService graphDb )
+	{
+		// Registers a shutdown hook for the Neo4j instance so that it
+		// shuts down nicely when the VM exits (even if you "Ctrl-C" the
+		// running application).
+		Runtime.getRuntime().addShutdownHook( new Thread()
+		{
+			@Override
+			public void run()
+			{
+				graphDb.shutdown();
+			}
+		} );
+	}
 }
