adf28f4e8997f9ee954e60f4bed8329760ed1e7a
Kathryn Rivard <krivard@cs.cmu.edu>
2013-12-03 09:56:02 -0500

    extended test for duplicate sig rules

--- test/edu/cmu/ml/praprolog/prove/DuplicateSignatureRuleTest.java
+++ test/edu/cmu/ml/praprolog/prove/DuplicateSignatureRuleTest.java
@@ -7,17 +7,17 @@ import java.util.Map;
 import org.junit.Test;
 
 /** 
- * When two goals in a rule have the same signature, ProPPR doesn't resolve their variables properly.
+ * Bug: When multiple goals in a rule have the same signature, ProPPR doesn't resolve their variables properly.
  * 
  * @author "Kathryn Mazaitis <krivard@cs.cmu.edu>"
  */
 public class DuplicateSignatureRuleTest {
 
 	@Test
-	public void test() {
+	public void test2() {
 		RuleComponent r = new RuleComponent();
 		/*
-		 * canExit(Player,Room) :- location(Player,Room),hasKey(Player,Room) .
+canExit(Player,Room) :- location(Player,Room),hasKey(Player,Room) .
 hasKey(Player,Room) :- class(Player,wizard) .
 hasKey(Player,Room) :- doorPuzzle(Room,Puzzle),solved(Player,Puzzle) .
 		 */
@@ -60,4 +60,92 @@ hasKey(Player,Room) :- doorPuzzle(Room,Puzzle),solved(Player,Puzzle) .
 		}
 	}
 
+	@Test
+	public void test3() {
+		RuleComponent r = new RuleComponent();
+		/*
+canExit(Player,Room) :- location(Player,Room),hasKey(Player,Room),trapsCleared(Player,Room) .
+hasKey(Player,Room) :- .
+trapsCleared(Player,Room) :- .
+		 */
+		r.add(new Rule(
+				new Goal("canExit","X","Y"),
+				new Goal("ability"),
+				new Goal("location","X","Y"),
+				new Goal("hasKey","X","Y"),
+				new Goal("trapsCleared","X","Y")));
+		r.add(new Rule(
+				new Goal("hasKey","P","Q"),
+				new Goal("unlocked")));
+		r.add(new Rule(
+				new Goal("trapsCleared","S","T"),
+				new Goal("traps")));
+		r.compile();
+		
+		GoalComponent g = new GoalComponent();
+		g.addFact(new Goal("doorPuzzle","kitchen","puzzle_kitchen"));
+		g.addFact(new Goal("doorPuzzle","parlor","puzzle_parlor"));
+		g.addFact(new Goal("class","steve","wizard"));
+		g.addFact(new Goal("location","steve","kitchen"));
+		g.addFact(new Goal("solved","steve","puzzle_parlor"));
+		
+		LogicProgram lp = new LogicProgram(new Component[] {r,g});
+		
+		ProPPRLogicProgramState state = new ProPPRLogicProgramState(Goal.decompile("canExit,steve,-1"));
+		
+		Prover p = new TracingDfsProver();
+		Map<LogicProgramState,Double> result = p.proveState(lp, state);
+		for (Map.Entry<LogicProgramState, Double> e : result.entrySet()) {
+			System.out.println(e.getValue()+"\t"+e.getKey());
+			System.out.println("\t"+e.getKey().description());
+			if (e.getKey().isSolution()) {
+				assertEquals("Steve not allowed to exit "+e.getKey().description()+".\n",
+						"-1=c[kitchen]",e.getKey().description());
+			}
+		}
+	}
+	@Test
+	public void test3b() {
+		RuleComponent r = new RuleComponent();
+		/*
+canExit(Player,Room) :- location(Player,Room),hasKey(Player,Room),trapsCleared(Player,Room) .
+hasKey(Player,Room) :- .
+trapsCleared(Player,Room) :- .
+		 */
+		r.add(new Rule(
+				new Goal("canExit","X","Y"),
+				new Goal("ability"),
+				new Goal("location","X","Y"),
+				new Goal("hasKey","X","Y"),
+				new Goal("trapsCleared","X","Y")));
+		r.add(new Rule(
+				new Goal("hasKey","X","Y"),
+				new Goal("unlocked")));
+		r.add(new Rule(
+				new Goal("trapsCleared","X","Y"),
+				new Goal("traps")));
+		r.compile();
+		
+		GoalComponent g = new GoalComponent();
+		g.addFact(new Goal("doorPuzzle","kitchen","puzzle_kitchen"));
+		g.addFact(new Goal("doorPuzzle","parlor","puzzle_parlor"));
+		g.addFact(new Goal("class","steve","wizard"));
+		g.addFact(new Goal("location","steve","kitchen"));
+		g.addFact(new Goal("solved","steve","puzzle_parlor"));
+		
+		LogicProgram lp = new LogicProgram(new Component[] {r,g});
+		
+		ProPPRLogicProgramState state = new ProPPRLogicProgramState(Goal.decompile("canExit,steve,-1"));
+		
+		Prover p = new TracingDfsProver();
+		Map<LogicProgramState,Double> result = p.proveState(lp, state);
+		for (Map.Entry<LogicProgramState, Double> e : result.entrySet()) {
+			System.out.println(e.getValue()+"\t"+e.getKey());
+			System.out.println("\t"+e.getKey().description());
+			if (e.getKey().isSolution()) {
+				assertEquals("Steve not allowed to exit "+e.getKey().description()+".\n",
+						"-1=c[kitchen]",e.getKey().description());
+			}
+		}
+	}
 }
