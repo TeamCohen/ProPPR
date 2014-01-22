9f2e5456bed0449c4a3b38721d2fba3025fab821
Kathryn Mazaitis <krivard@cs.cmu.edu>
2014-01-22 10:22:33 -0500

    Merged with svn 2004

---
+++ src/edu/cmu/ml/praprolog/trove/Experiment.java
@@ -0,0 +1,10 @@
+package edu.cmu.ml.praprolog.trove;
+
+import java.util.Map;
+
+import edu.cmu.ml.praprolog.trove.learn.L2PosNegLossTrainedSRW;
+import edu.cmu.ml.praprolog.util.Dictionary;
+
+public class Experiment {
+	
+}
