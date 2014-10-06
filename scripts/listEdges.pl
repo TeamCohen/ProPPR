35333a9021a3f1ffefe1d0de8e5ed4f4b70433f3
Kathryn Mazaitis <krivard@cs.cmu.edu>
2014-10-06 15:47:45 -0400

    scripts for processing cooked files

---
+++ scripts/listEdges.pl
@@ -0,0 +1,77 @@
+#!/usr/bin/perl
+
+my $cookedFile=shift;
+my $keyFile=shift;
+defined($keyFile) or die "Usage:\n\ttrainOrTest.examples.cooked trainOrTest.examples.graphKey > query_feature_src_dst.txt\n";
+
+open(my $cF,"<$cookedFile") or die "Couldn't open file $cookedFile for reading:\n$!\n";
+open(my $kF,"<$keyFile") or die "Couldn't open key file $keyFile for reading:\n$!\n";
+
+my $c=0;
+my $k=0;
+my $kcache=<$kF> or die "End of key file reached before I could even get started\n";
+$kcache =~ s/-[0-9][0-9]*/_/;
+# for each line of the cooked file, gather all the edges by feature
+while(<$cF>) {
+    chomp;
+    $c++;
+    my %edgedata; # feature -> ( [src,dst] )
+    my $ne=0;
+    $_=~s/-[0-9][0-9]*/_/;
+    my ($query,$foo2,$foo3,$foo4,$foo5,$foo6,$featurestr,@edges) = split("\t");
+    my @features = split(":",$featurestr);
+    
+    my $searchi = -1;
+    foreach my $feature (@features) {
+	$edgedata{$feature} = ();
+    }
+    foreach my $e (@edges) {
+	my @foo = split(":",$e);
+	my @bar = split("->",$foo[0]);
+	foreach my $f ( split(",", $foo[1]) ) {
+	    push(@{$edgedata{$features[$f]}}, \@bar);
+	}
+    }
+    
+    # then gather the node labels for this query
+    $ne=0;
+    my @nodeNames;
+    while(1) {
+	my $key = $kcache;
+	$k++;
+	chomp($key);
+	my ($kquery,$nodeid,$name) = split("\t",$key);
+	($kquery eq $query) or die "Mismatch between line $c of cooked file and line $k of key file:\n$query\n$kquery\n";
+	#print "$k: $nodeid -> $name\n";
+	$nodeNames[$nodeid] = $name;
+	$ne++;
+	#print "$k: $kcache";
+	
+	# preload next line:
+	last unless ($kcache = <$kF>);
+	$kcache =~ s/-[0-9][0-9]*/_/;
+	#print "\t$k+1: $kcache";
+	chomp($kcache);
+	($kquery,$nodeid,$name) = split("\t",$kcache);
+	#my $yes="keep going";
+	#$yes="stop" if ($kquery ne $query);
+	#print "\t$k+1: $yes\n";
+	last if ($kquery ne $query);
+    } 
+    #print "$ne node names retrieved at line $k of key file\n";
+
+    # then print the edge data
+    $ne=0;
+    foreach my $feature (keys %edgedata) {
+	foreach my $e_ref ( @{$edgedata{$feature}} ) {
+	    #print "$e_ref\n";
+	    my $src = $nodeNames[$e_ref->[0]];
+	    my $dst = $nodeNames[$e_ref->[1]];
+	    print "$query\t$feature\t$e_ref->[0]\t$e_ref->[1]\t$src\t$dst\n";
+	    $ne++;
+	}
+    }
+}
+
+close($cF);
+close($kF);
---
+++ scripts/solutionFeatures.pl
@@ -0,0 +1,102 @@
+#!/usr/bin/perl
+
+my $cookedFile = shift;
+
+defined($cookedFile) or die "Usage:\n\texamples.cooked > output\n";
+
+open(my $cf,"<$cookedFile") or die "Couldn't open cooked input file $cookedFile:\n$!\n";
+
+my %all_labels = ();
+my $line=0;
+while(<$cf>) {
+    chomp;
+    $line++;
+    my ($query,$qid,$posid,$negid,$nodesize,$edgesize,$featurestr,@edges) = split("\t");
+    my @features = split(":",$featurestr);
+    my @pos = split(",",$posid);
+    my @neg = split(",",$negid);
+
+    # build graph
+    my %graph;
+    foreach my $e (@edges) {
+	my @parts = split (":",$e);
+	my @neighbors = split("->",$parts[0]);
+	my @labels = split(",",$parts[1]);
+	foreach $el (@labels) {
+	    push ( @{$graph{$neighbors[0]}{$el}}, $neighbors[1] );
+	}
+    }
+
+    my %is_pos=();
+    @is_pos{@pos} = (1) x scalar @pos;
+    my %is_neg=();
+    @is_neg{@neg} = (1) x scalar @neg;
+    my %label_data = ();
+
+    # walk from query node
+#    print "pos ids:\t$posid\n";
+#    print "neg ids:\t$negid\n";
+    my %seen = ();
+    walk($qid,\%graph,\%seen,[],\%is_pos,\%is_neg,\%label_data);
+#    print "label\t#pos\t#neg\n";
+    foreach my $el (keys %label_data) { 
+#	print "$el\t$label_data{$el}{pos}\t$label_data{$el}{neg}\n"; 
+	$fn = $features[$el];
+	$all_labels{$fn}{count}{pos} += $label_data{$el}{pos};
+	$all_labels{$fn}{count}{neg} += $label_data{$el}{neg};
+	if ($label_data{$el}{pos} > 0) {
+	    push @{$all_labels{$fn}{queries}{pos}},$line;
+	}
+	if ($label_data{$el}{neg} > 0) {
+	    push @{$all_labels{$fn}{queries}{neg}},$line;
+	}
+    }
+    if ( ( $line % 50) == 0) {
+	print STDERR "$line queries read...\r";
+    }
+
+#    last;
+    # print graph
+#    foreach my $src (keys %graph) {
+#	foreach my $label (keys %{$graph{$src}}) {
+#	    foreach my $dst (@{$graph{$src}{$label}}) {
+#		print "$src\t$label\t$dst\n";
+#	    }
+#	}
+#    }
+#    last;
+}
+close($cf);
+
+print "\n";
+foreach my $el (keys %all_labels) {
+    print "$el\t$all_labels{$el}{count}{pos}\t$all_labels{$el}{count}{neg}\n";
+}
+
+
+sub walk {
+    my ($node, $graph, $seen, $path, $is_pos, $is_neg, $label_data) = @_;
+#    print "$node:";
+#    foreach my $el (@{$path}) {print "$el,";}
+#    print "\n";
+    $seen->{$node}=1;
+    check($node,$is_pos,"pos",$path,$label_data);
+    check($node,$is_neg,"neg",$path,$label_data);
+    foreach my $label (keys %{$graph->{$node}}) {
+	push @{$path},$label;
+	foreach my $next (@{$graph->{$node}{$label}}) {
+	    next if ($seen->{$next});
+	    walk($next,$graph,$seen,$path,$is_pos,$is_neg,$label_data);
+	}
+	pop @{$path};
+    }
+}
+
+sub check {
+    my ($node, $is_x, $key, $path, $label_data) = @_;
+    if ($is_x->{$node}) {
+	foreach my $el (@{$path}) {
+	    $label_data->{$el}{$key}++;
+	}
+    }
+}
