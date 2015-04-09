543cb0c64e2726cab818654375d29eea66ac0318
Kathryn Mazaitis <krivard@cs.cmu.edu>
2015-04-09 11:46:28 -0400

    playing with some time optimizations in graph building

--- src/java/main/edu/cmu/ml/proppr/graph/LearningGraphBuilder.java
+++ src/java/main/edu/cmu/ml/proppr/graph/LearningGraphBuilder.java
@@ -5,11 +5,11 @@ import gnu.trove.map.TObjectDoubleMap;
 import gnu.trove.map.hash.TObjectDoubleHashMap;
 
 public abstract class LearningGraphBuilder {
-	public static final String FEATURE_INDEX_DELIM = ":";
-	public static final String EDGE_FEATURE_DELIM = ":";
+	public static final char FEATURE_INDEX_DELIM = ':';
+	public static final char EDGE_FEATURE_DELIM = ':';
 	public static final String SRC_DST_DELIM = "->";
-	public static final String FEATURE_DELIM = ",";
-	public static final String FEATURE_WEIGHT_DELIM = "@";
+	public static final char FEATURE_DELIM = ',';
+	public static final char FEATURE_WEIGHT_DELIM = '@';
 	
 	public abstract LearningGraphBuilder copy();
 	public abstract LearningGraph create();
@@ -18,44 +18,78 @@ public abstract class LearningGraphBuilder {
 	public abstract void freeze(LearningGraph g);
 	public abstract void index(int i0);
 	public LearningGraph deserialize(String string) throws GraphFormatException {
-		String[] parts = string.split("\t",4);
-		if (parts.length != 4) {
-			throw new GraphFormatException("Only "+parts.length+" tsv fields in graph; need 4 distinct parts:"+string);
+		int[] parts = new int[3];
+		parts[0] = string.indexOf('\t');
+		for (int i=1; i<parts.length; i++) parts[i] = string.indexOf('\t', parts[i-1]+1);
+		
+		if (parts[2] == -1) {
+			throw new GraphFormatException("Need 4 distinct tsv fields in the graph:"+string);
 		}
 
-		int nodeSize = Integer.parseInt(parts[0]);
-		int edgeSize = Integer.parseInt(parts[1]);
+		int nodeSize = Integer.parseInt(string.substring(0, parts[0]));
+		int edgeSize = Integer.parseInt(string.substring(parts[0]+1,parts[1]));
 		LearningGraph graph = create();
 		index(1);
 		setGraphSize(graph, nodeSize, edgeSize);
 
 		String[] featureList;
-		if(parts[2].equals("-")) featureList = new String[0];
-		else featureList = parts[2].split(FEATURE_INDEX_DELIM);
+		if(parts[2]-parts[1]==2 && string.charAt(parts[1]+1) == ('-')) featureList = new String[0];
+		else featureList = string.substring(parts[1]+1,parts[2]).split(String.valueOf(FEATURE_INDEX_DELIM));
 
-		for (String p : parts[3].split("\t")) {
-			String[] pair = p.split(EDGE_FEATURE_DELIM);
-			String edgeStr = pair[0], featStr = pair[1];
+		handleEdgeList(string, parts[2], graph, featureList);
+		
+		freeze(graph); // might be slow? but saves memory
+		return graph;
+	}
 
-			String[] raw = edgeStr.split(SRC_DST_DELIM);
-			int[] nodes = new int[raw.length];
-			for (int i=0; i<raw.length; i++) { nodes[i] = Integer.parseInt(raw[i]); }
+	private void handleEdgeList(String string, int start, LearningGraph graph, String[] featureList) throws GraphFormatException {
+
+//		int last = parts[3], next;
+		//for (String p : string.substring(parts[3]+1).split("\t")) {
+		for (int last = start, next=0; true ; last=next, next = string.indexOf('\t',last+1) ) {
+			if (next == -1) next = string.length();
+			if (next == last) break;
+			
+			int edgeDelim = string.indexOf(EDGE_FEATURE_DELIM,last+1);
+//			String[] pair = p.split(EDGE_FEATURE_DELIM);
+//			String edgeStr = pair[0], featStr = pair[1];
+
+//			String[] raw = edgeStr.split(SRC_DST_DELIM);
+			int nodeDelim = string.indexOf(SRC_DST_DELIM, last+1);
+			int[] nodes = {Integer.parseInt(string.substring(last+1,nodeDelim)), 
+					Integer.parseInt(string.substring(nodeDelim+SRC_DST_DELIM.length(), edgeDelim)) };
+//			int[] nodes = new int[raw.length];
+//			for (int i=0; i<raw.length; i++) { nodes[i] = Integer.parseInt(raw[i]); }
 
 			TObjectDoubleMap<String> fd = new TObjectDoubleHashMap<String>();
-			for (String f : featStr.split(FEATURE_DELIM)) {
-				if (featureList.length > 0) {
-					String[] fw = f.split(FEATURE_WEIGHT_DELIM);
-					fd.put(featureList[Integer.parseInt(fw[0])-1],fw.length>1?Double.parseDouble(fw[1]):1.0);
-				} else {
-					fd.put(f,1.0);
-				}
-			}
+			handleEdgeFeatures(string, edgeDelim, next, fd, featureList);
 			if (fd.isEmpty()) {
 				throw new GraphFormatException("Can't have no features on an edge for ("+nodes[0]+", "+nodes[1]+")");
 			}
 			addOutlink(graph, nodes[0], new RWOutlink(fd, nodes[1]));
 		}
-		freeze(graph); // might be slow? but saves memory
-		return graph;
+	}
+	
+	private void handleEdgeFeatures(String string, int start, int next, TObjectDoubleMap<String> fd, String[] featureList) {
+
+//		for (String f : featStr.split(FEATURE_DELIM)) {
+		for (int lastF = start, nextF = 0; true ; lastF=nextF, nextF = Math.min(next, string.indexOf(FEATURE_DELIM, lastF+1)) ) {
+			if (nextF == -1) nextF = next;
+			if (nextF == lastF) break;
+			
+			double wt = 1.0;
+			String f = string.substring(lastF+1, nextF);
+			if (featureList.length > 0) {
+				int weightDelim = f.indexOf(FEATURE_WEIGHT_DELIM);
+				if (weightDelim>0) { // @ is never the first char in the feature string
+					wt = Double.parseDouble(f.substring(weightDelim+1));
+					f = f.substring(0,weightDelim);
+				}
+//				String[] fw = f.split(FEATURE_WEIGHT_DELIM);
+				fd.put(featureList[Integer.parseInt(f)-1],wt);
+			} else {
+				fd.put(f,wt);
+			}
+		}
 	}
 }
--- src/java/test/edu/cmu/ml/proppr/graph/LearningGraphTest.java
+++ src/java/test/edu/cmu/ml/proppr/graph/LearningGraphTest.java
@@ -2,6 +2,11 @@ package edu.cmu.ml.proppr.graph;
 
 import static org.junit.Assert.*;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.LineNumberReader;
+
 import org.junit.Test;
 
 import edu.cmu.ml.proppr.graph.LearningGraph.GraphFormatException;
@@ -19,4 +24,13 @@ public class LearningGraphTest {
 		assertEquals("#features on 1->2",1,g.edge_labels_hi[0] - g.edge_labels_lo[0]);
 	}
 
+	
+	@Test
+	public void testCoraArray() throws GraphFormatException, IOException {
+		LineNumberReader r = new LineNumberReader(new FileReader("examples/cora/train.examples.1.grounded"));
+		String s = r.readLine().split("\t",5)[4];
+		r.close();
+		LearningGraphBuilder b = new ArrayLearningGraph.ArrayLearningGraphBuilder();
+		ArrayLearningGraph g = (ArrayLearningGraph) b.deserialize(s);
+	}
 }
