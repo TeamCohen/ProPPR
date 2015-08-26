b4ace06e3db5800fda59f0b0303ad1ec20b67be0
Kathryn Mazaitis <krivard@cs.cmu.edu>
2015-08-26 15:52:27 -0400

    Configuration bug workaround?

--- src/java/main/org/apache/commons/cli/PermissiveParser.java
+++ src/java/main/org/apache/commons/cli/PermissiveParser.java
@@ -4,25 +4,26 @@ import java.util.ListIterator;
 import java.util.Set;
 import java.util.TreeSet;
 
-public class PermissiveParser extends BasicParser {
-	private boolean ignoreUnrecognizedOption;
-	Set<String> unrecognizedOptions;
+public class PermissiveParser extends DefaultParser {
+//	private boolean ignoreUnrecognizedOption;
+//	Set<String> unrecognizedOptions;
 
     public PermissiveParser(final boolean ignoreUnrecognizedOption) {
-        this.ignoreUnrecognizedOption = ignoreUnrecognizedOption;
-        this.unrecognizedOptions = new TreeSet<String>();
+    	this.stopAtNonOption=false;
+//        this.ignoreUnrecognizedOption = ignoreUnrecognizedOption;
+//        this.unrecognizedOptions = new TreeSet<String>();
     }
 
-    @Override
-    protected void processOption(final String arg, final ListIterator iter) throws ParseException {
-        boolean hasOption = getOptions().hasOption(arg);
-        if (hasOption || !ignoreUnrecognizedOption) {
-            super.processOption(arg, iter);
-        } else if (ignoreUnrecognizedOption) {
-        	this.unrecognizedOptions.add(arg);
-        }
-    }
+//    @Override
+//    protected void processOption(final String arg, final ListIterator iter) throws ParseException {
+//        boolean hasOption = this.options.hasOption(arg);
+//        if (hasOption || !ignoreUnrecognizedOption) {
+//            super.processOption(arg, iter);
+//        } else if (ignoreUnrecognizedOption) {
+//        	this.unrecognizedOptions.add(arg);
+//        }
+//    }
     
-    public Set<String> getUnrecognizedOptions() { return this.unrecognizedOptions; }
-    public boolean hasUnrecognizedOptions() { return !this.unrecognizedOptions.isEmpty(); }
+//    public Set<String> getUnrecognizedOptions() { return this.unrecognizedOptions; }
+//    public boolean hasUnrecognizedOptions() { return !this.unrecognizedOptions.isEmpty(); }
 }
