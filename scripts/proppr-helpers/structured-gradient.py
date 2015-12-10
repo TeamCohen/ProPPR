990aecd3cc8b8a1fdb1d1f59bee5b5447e004b48
William Cohen <wcohen@cs.cmu.edu>
2015-12-10 13:01:12 -0500

    refactoring

--- scripts/proppr-helpers/structured-gradient.py
+++ scripts/proppr-helpers/structured-gradient.py
@@ -1,11 +1,10 @@
-# old code calls Make to build gradents, etc
-# seems way to complicated....
-
 import sys
+import os
 import re
 import getopt
 import random
 import logging
+import subprocess
 import collections
 
 
@@ -89,10 +88,127 @@ def gradientToRules(src,dst,opts):
     fp = open(dst,'w')
     fp.write("\n".join(rules) + "\n")
 
+def stucturedGradient(src,dst,opts):
+    #work out inputs/outputs
+    exampleFile = src
+    learnedRuleFile = dst
+    backgroundFile = optdict['--src2']
+    exampleStem = optdict['--stem']
+
+    #get the interpreter and compile it, then ground the examples
+    interpFile = _getResourceFile(opts, "sg-interp-train.ppr")
+    invokeProppr(opts,'compile',interpFile)
+    programFileList =  interpFile[:-4]+'.wam:'+backgroundFile
+    invokeProppr(opts,'ground',exampleFile,exampleFile+".grounded",'--programFiles',programFileList)
+
+    #store gradient in a temp file
+    gradientFile = exampleStem+'.gradient'
+    invokeProppr(opts,'gradient',exampleFile+".grounded",gradientFile,'--epochs','0')
+
+    #convert the gradient features to rules interp(R,X,Y) :- BODY where BODY contains calls to rel(R,X,Y).l
+    gradientToRules(gradientFile, learnedRuleFile, {'--lhs':'interp','--rhs':'rel'})
+
+
+def iterativeStucturedGradient(src,dst,opts):
+    #work out inputs/outputs
+    exampleFile = src
+    learnedRuleFile = dst
+    backgroundFile = optdict['--src2']
+    exampleStem = optdict['--stem']
+    numIters = int(optdict['--numIters'])
+
+    #copy the initial interpreter to this directory
+    interpFile = _getResourceFile(opts, "sg-interp-train.ppr")
+    learnedRuleFiles = []
+
+    #iteratively learn
+    for i in range(numIters):
+        logging.info('training pass %i' % i)
+
+        #create the i-th interpreter, which contains the basic interpreter rules, plus all the learned rules
+        interpFile = 'sg-interp_n%02d.ppr' % i
+        numAddedThisRound = _appendUniqLines(['sg-interp-train.ppr']+learnedRuleFiles,interpFile)
+        if numAddedThisRound==0:
+            logging.info('no new rules learned in previous iteration - stopping')
+            break
+        _catfile(interpFile,'Interpreter used at round %d' % i)
+
+        #compile the interpreter
+        invokeProppr(opts,'compile',interpFile)
+
+        #ground the examples using the interpreter + learned rules
+        programFileList =  interpFile[:-4]+'.wam:'+backgroundFile
+        invokeProppr(opts,'ground',exampleFile,exampleFile+".grounded",'--programFiles',programFileList)
+
+        #compute the gradient
+        gradientFile = exampleStem+'_n%02d.gradient' % i
+        invokeProppr(opts,'gradient',exampleFile+".grounded",gradientFile,'--epochs','0')
+
+        #convert the gradient features to rules interp(R,X,Y) :- BODY where BODY contains calls to rel(R,X,Y).
+        nextLearnedRuleFile = '%s-learned_n%02d.ppr' % (exampleStem,i)
+        gradientToRules(gradientFile,nextLearnedRuleFile,opts)
+        logging.info('Created rule file ' + nextLearnedRuleFile)
+        _catfile(nextLearnedRuleFile,'Rules learned in round %d' % i)
+
+        #add this to the list of learned rules
+        learnedRuleFiles.append(nextLearnedRuleFile)
+
+    #concatenate all the learned rules, replacing the interpreter with a new one
+    testInterpFile = _getResourceFile(opts, "sg-interp-test.ppr")
+    _appendUniqLines([testInterpFile] + learnedRuleFiles,learnedRuleFile)
+
+def _getResourceFile(opts,filename):
+    if '--n' not in opts: #not dry run
+        src = os.path.join( '%s/scripts/proppr-helpers/%s' % (os.environ['PROPPR'], filename))
+        dst = filename
+        fp = open(dst,'w')
+        for line in open(src):
+            fp.write(line)
+        logging.info('copied %s to current directory' % src)
+    return filename
+
+def _appendUniqLines(inputs,output):
+   previousLines = set()
+   fp = open(output,'w')
+   for f in inputs:
+      numAddedFromLastFile = 0
+      for line in open(f): 
+         if line not in previousLines:
+            previousLines.add(line)
+            numAddedFromLastFile += 1
+            fp.write(line)
+   fp.close()
+   return numAddedFromLastFile
+
+def _catfile(fileName,msg):
+    """Print out a created file - for  debugging"""
+    print msg
+    print '+------------------------------'
+    for line in open(fileName):
+        print ' |',line,
+    print '+------------------------------'
+
+def invokeProppr(opts,*args):
+    procArgs = ['%s/scripts/proppr' % os.environ['PROPPR']]
+    #deal with proppr's global options
+    if '--C' in opts:
+        procArgs.extend(['--C', optdict['--C']])
+    if '--n' in optdict:
+        procArgs.extend(['--n'])
+    procArgs.extend(args)
+    if '--n' not in opts: #not dry run
+        logging.info('calling: ' + ' '.join(procArgs))
+        stat = subprocess.call(procArgs)
+        if stat:
+            logging.info(('call failed (status %d): ' % stat) + ' '.join(procArgs))
+            sys.exit(stat) #propagate failure
+
 if __name__=="__main__":
     logging.basicConfig(level=logging.INFO)
-    argspec = ["com=","src=", "dst=",
+    argspec = ["com=","src=", "dst=", 
+               "C=", "n", #global proppr opts
                "lhs=", "rhs=", #for gradientToRules
+               "src2=", "numIters=", "stem=", #for iterativeStucturedGradient, structuredGradient
     ]
     try:
         optlist,args = getopt.getopt(sys.argv[1:], 'x', argspec)
@@ -113,5 +229,9 @@ if __name__=="__main__":
         relationsToExamples(src,dst,optdict)
     elif subcommand=='grad2ppr':
         gradientToRules(src,dst,optdict)
+    elif subcommand=='isg-train':
+        iterativeStucturedGradient(src,dst,optdict)
+    elif subcommand=='sg-train':
+        stucturedGradient(src,dst,optdict)
     else:
         assert False,'does not compute '+subcommand
